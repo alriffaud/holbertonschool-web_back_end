b3493f9f608a28c1b81ff337e4cfaeed8b83d6e5
Alberto Riffaud <alriffaud@gmail.com>
2024-04-26 12:56:23 -0300

    Creation of 5-building.js file.

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,23 @@
+// This module implement a class named Building.
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    if (typeof newSqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = newSqft;
+  }
+}
