38dd3064d76af85ae9ad80641801a7d90d2bd20a
Alberto Riffaud <alriffaud@gmail.com>
2024-05-02 09:18:58 -0300

    Creation of 9-try.js file.

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,10 @@
+// This script defines a function named.
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    queue.push(mathFunction(), 'Guardrail was processed');
+  } catch (error) {
+    queue.push(error.toString(), 'Guardrail was processed');
+  }
+  return (queue);
+}
