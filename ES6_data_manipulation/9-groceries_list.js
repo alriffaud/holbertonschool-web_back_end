60ef0ffe881c3f5c86c9e1d070a18b3210072e2b
Alberto Riffaud <alriffaud@gmail.com>
2024-05-03 12:25:55 -0300

    Creation of 9-groceries_list.js file.

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+// This script creates  a function named groceriesList that returns a map of groceries.
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
